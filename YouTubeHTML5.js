3644cd5960431bb220595c80df6626f98bfcd15a
Chris Robins <chrisrobins@gmail.com>
2014-01-03 12:09:05 +0000

    default download attribute

--- YouTubeHTML5.js
+++ YouTubeHTML5.js
@@ -332,6 +332,7 @@ UI_RELOAD_BUTTON.addEventListener("click", function () {
 
 UI_DOWNLOAD_LINK.addEventListener("click", function () {
     var x = "",
+        t = document.title.replace(/^\u25B6\s/, "").replace(/^YouTube\s-\s/, ""),
         i = UI_SOURCE_SELECT.options[UI_SOURCE_SELECT.selectedIndex].dataset.itag;
     if (i) {
         switch (i) {
@@ -358,7 +359,7 @@ UI_DOWNLOAD_LINK.addEventListener("click", function () {
             break;
         }
     }
-    this.download = document.title.replace(/^\u25B6\s/, "").replace(/\s-\sYouTube$/, "") + x;
+    this.download =  t + x || "YouTube video";
     this.href = HTML5_VIDEO.src;
 });
 
